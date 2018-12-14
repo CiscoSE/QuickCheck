b9a0d7663c33d3a9bd92ad91a8bc2ce84a4effb6
Keller McBride <keller.mcbride@gmail.com>
2018-12-14 15:42:02 -0600

    Edited Main to comply with python structure

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-This is a test of the emergency broadcasting system.  This is only a test. If this had been a real emergency, you would have been instructed to tune to the nearest radio station.
+#!/usr/bin/env python
+"""Module docstring."""
+
+
+# Imports
+
+
+
+# Module Constants
+
+
+# Module "Global" Variables
+
+
+# Module Functions and Classes
+def main(*args):
+    """My main script function.
+    print('hello world')
+
+
+# Check to see if this file is the "__main__" script being executed
+if __name__ == '__main__':
+    _, *script_args = sys.argv
+    main(*script_args)
